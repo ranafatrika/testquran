0fa7b597edf324ef2758835524e9bfa035056907
ranafatrika <12050120472@students.uin-suska.ac.id>
2022-05-10 13:08:21 +0700

    Initial commit

---
+++ src/components/navbar.vue
@@ -0,0 +1,53 @@
+<script>
+export default{
+  name: "NavBar",
+};
+</script>
+
+<template>
+  <nav class="navbar navbar-dark bg-primary fixed-top">
+    <div class="container-fluid">
+      <div class="navbar-header">
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarTogglerDemo02" aria-controls="navbarTogglerDemo02" aria-expanded="false" aria-label="Toggle navigation">
+          <span class="navbar-toggler-icon"></span>
+        </button>
+      </div>
+      <a class="navbar-brand" href="#">MY AL-QURAN | by quran.com</a>
+      <img src="../assets/al5.jpg" alt="" width="40" height="30">
+      <div class="collapse navbar-collapse" id="navbarTogglerDemo02">
+        <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+          <li class="nav-item">
+            <Router-link class="nav-link" aria-current="page" to="/">Home</Router-link>
+          </li>
+          <li class="nav-item">
+            <Router-link class="nav-link" to="/about">About</Router-link>
+          </li>
+          <li class="nav-item">
+            <Router-link class="nav-link" to="/ayat Al-qur'an">Al-Qur'an</Router-link>
+          </li>
+          <li class="nav-item dropdown">
+            <a class="nav-link dropdown-toggle" data-bs-toggle="dropdown" href="#" role="button"
+               aria-expanded="false">Surah</a>
+            <ul class="dropdown-menu">
+              <li v-for="surah in surahs" :key="surah.id">
+                <router-link :to="{ name: 'surahs', params: { id: surah.id } }" class="dropdown-item">{{
+                    surah.name_complex
+                  }}</router-link>
+              </li>
+            </ul>
+          </li>
+          <li class="nav-item">
+            <Router-link class="nav-link" to="/randomAyat">Random Ayat</Router-link>
+          </li>
+        </ul>
+      </div>
+    </div>
+  </nav>
+</template>
+<style>
+.navbar-brand {
+  text-align: center;
+  font-family: "Comic Sans MS";
+}
+</style>
+
---
+++ src/views/HomeView.vue
@@ -0,0 +1,64 @@
+<script setup>
+import Navbar from "../components/Navbar.vue";
+import { onMounted } from "vue";
+
+onMounted(() => {
+  document.getElementsByTagName("body")[0].style.backgroundColor =" #87cefa";
+  document.getElementsByTagName("body")[0].style.backgroundSize ="cover";
+});
+</script>
+
+<template>
+  <navbar />
+  <section class="judul">
+    <div class="container py-5">
+      <div class="row">
+        <div class="">
+          <div class="col-lg-12 text-center">
+            <h1>
+              Tugas UTS mata kuliah Pemrograman Web
+            </h1>
+            <button type="button" class="btn btn-primary" data-bs-toggle="tooltip" data-bs-html="true" title="Github"> <i class="bi bi-heart-fill text-danger"></i>  <i class="bi text-danger"></i>  <a href="https://github.com/ranafatrika/pemprograman-web-uts-RanaFatrika.git" class="text-white" >View Code</a>
+            </button>
+
+            <div class="ratio ratio-16x9">
+              <iframe width="560" height="315" src="https://www.youtube.com/embed/8GBLBWbEM7w" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </section>
+
+
+</template>
+
+<style scoped>
+
+.judul{
+  background: #87cefa;
+  font-weight: 20;
+}
+.judul h1{
+  margin-top: 20px;
+  font-size: 3rem;
+  text-align: center;
+  font-weight: bold;
+  font-family: "poppins";
+
+}
+.judul h2 {
+  text-align: center;
+  font-size: 8rem;
+  font-family: "Segoe UI";
+  font-style: italic;
+}
+button{
+  font-family: "poppins";
+  color: #090808;
+}
+.ratio{
+  margin-top: 30px;
+  margin-bottom: 100%;
+}
+</style>
